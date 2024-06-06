ed0d92c6ab09fc323e2f16e456cdd753b63258c0
Olaf <ojaskra@gmail.com>
2024-06-06 23:00:11 +0200

    recognition functions cleanup

--- content-script.js
+++ content-script.js
@@ -1,50 +1,33 @@
+const initialize = () => {
+    const SpeechRecognition = window.webkitSpeechRecognition;
+    const SpeechGrammarList = window.webkitSpeechGrammarList;
+    
+    const grammar =
+        "#JSGF V1.0; grammar commands; public <command> = play | pause | fullscreen";
+    const recognition = new SpeechRecognition();
+    const speechRecognitionList = new SpeechGrammarList();
+    speechRecognitionList.addFromString(grammar, 1);
+    recognition.grammars = speechRecognitionList;
+    recognition.lang = "en-US";
 
-// (async () => {
-//     const SpeechRecognition = window.webkitSpeechRecognition;
-//     const SpeechGrammarList = window.webkitSpeechGrammarList
-//     const SpeechRecognitionEvent = window.webkitSpeechRecognitionEvent
-//     console.log(window)
-    console.log('content-script has been injected');
-    navigator.mediaDevices
-        .getUserMedia({ audio: true })
+    return recognition;
+}
+
+const RECOGNITION = initialize();
+RECOGNITION.start();
+
+const handleVoiceCommand = (e) => {
+    const res = e.results[0][0].transcript;
+    console.log(res);
+}
+
+RECOGNITION.onstart = () => console.log('Recognition started.');
+RECOGNITION.onend = () => !RECOGNITION.manualStop && setTimeout(() => RECOGNITION.start(), 100);
+
+RECOGNITION.onresult = handleVoiceCommand;
 
 
-//     const grammar =
-//         "#JSGF V1.0; grammar commands; public <command> = play | pause | fullscreen";
-//     const recognition = new SpeechRecognition();
-//     const speechRecognitionList = new SpeechGrammarList();
-//     speechRecognitionList.addFromString(grammar, 1);
-//     recognition.grammars = speechRecognitionList;
-//     recognition.continuous = false;
-//     recognition.lang = "en-US";
-//     recognition.interimResults = false;
-//     recognition.maxAlternatives = 1;
 
-//     recognition.start();
 
-//     recognition.onspeechstart = (event) => {
-//         //const color = event.results[0][0].transcript;
-//         console.log('speech detected')
-//     };
-
-//     recognition.onsoundstart = (event) => {
-//         //const color = event.results[0][0].transcript;
-//         console.log('sound detected')
-//     };
-
-//     recognition.onspeechend = (e) => {
-//         recognition.stop();
-//         console.log('speech end')
-//     }
-
-//     recognition.onresult = (event) => {
-//         const color = event.results
-//         console.log(color)
-//     }
-
-//     recognition.onnomatch = function (event) {
-//         console.log('didnt recognize')
-//     }
-// })()
 
 
